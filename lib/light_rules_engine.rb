942089c4fb69808978b264420c2b42030cc6278d
Pawel Niemczyk <p.niemczyk@pilot.co>
2016-10-24 02:52:49 +0200

    prototype

--- lib/light_rules_engine.rb
+++ lib/light_rules_engine.rb
@@ -1,5 +1,162 @@
 require 'light_rules_engine/version'
 
 module LightRulesEngine
-  # Your code goes here...
+  module Operators
+    def self.find(name)
+      class_name = classify_string(name.to_s)
+      self.const_get(class_name)
+    end
+
+    def self.classify_string(string)
+      string = string.sub(/^[a-z\d]*/) { $&.capitalize }
+      string.gsub(/(?:_|(\/))([a-z\d]*)/) { "#{$1}#{$2.capitalize}" }.gsub('/', '::')
+      string.sub(/.*\./, '')
+    end
+
+    class All
+      def self.result(*args)
+        args.all?
+      end
+    end
+
+    class Any
+      def self.result(*args)
+        args.any?
+      end
+    end
+
+    class Gt
+      def self.result(*args)
+        args[0] > args[1]
+      end
+    end
+
+    class Lt
+      def self.result(*args)
+        args[0] < args[1]
+      end
+    end
+
+    class Eq
+      def self.result(*args)
+        args.all? { |x| x == args[0] }
+      end
+    end
+
+    class Range
+      def self.result(*args)
+        from  = args[0]
+        to    = args[1]
+        value = args[2]
+        from <= value && to >= value
+      end
+    end
+  end
+
+  ALL_CONSTS = { 'THE_NUMBER' => 1 }.freeze
+
+  class ValueResolver
+    def initialize(data_provider:, operation_context_class:)
+      @data_provider           = data_provider
+      @operation_context_class = operation_context_class
+    end
+
+    def resolve(value)
+      kind = value[:kind]
+      send("process_#{kind}", value)
+    end
+
+    private
+
+    attr_reader :operation_context_class, :data_provider
+
+    def process_operator(value)
+      operation_context_class.new(value, data_provider).result
+    end
+
+    def process_data(value)
+      data_provider.value_for(value[:type])
+    end
+
+    def process_const(value)
+      ALL_CONSTS[value[:value]]
+    end
+
+    def process_value(value)
+      value[:value]
+    end
+  end
+
+  class OperatorContxt
+    def initialize(operator_hash, data_provider)
+      @operator_hash = operator_hash
+      @data_provider = data_provider
+    end
+
+    def operator
+      @operator ||= Operators.find(operator_hash[:type])
+    end
+
+    def values
+      operator_hash[:values].map(&method(:resolve))
+    end
+
+    def result
+      operator.result(*values)
+    end
+
+    private
+
+    attr_reader :operator_hash, :data_provider
+
+    def resolve(value)
+      ValueResolver.new(data_provider: data_provider, operation_context_class: self.class).resolve(value)
+    end
+  end
+
+  class DataProvider
+    def initialize(data)
+      @data = data
+    end
+
+    def value_for(keyword)
+      send_methods = keyword.split('.').compact
+      send_methods.inject(data) do |object, m|
+        arg_var_one = m[/\(.*?\)/]
+        arg_var_two = m[/\[.*?\]/]
+        arg = (arg_var_one || arg_var_two)
+        m.gsub!(arg.to_s, '')
+        next unless object.respond_to?(m)
+        next object.public_send(m) unless arg
+        arg.delete!("\"'[()]")
+        next object.public_send(m, arg) if arg_var_one
+        arg = Integer(arg) rescue arg
+        object.public_send(m).public_send(:[], arg)
+      end
+    end
+
+    private
+
+    attr_reader :data
+  end
+
+  class Processor
+    def initialize(data_container:, conditions:)
+      @data_container = data_container
+      @conditions     = conditions
+    end
+
+    def applable?
+      return false unless conditions[:kind] == :operator
+      OperatorContxt.new(conditions, data_provider).result
+    end
+
+    private
+
+    attr_reader :data_container, :conditions
+
+    def data_provider
+      @data_provider ||= DataProvider.new(data_container)
+    end
+  end
 end
--- spec/light_rules_engine_spec.rb
+++ spec/light_rules_engine_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
+class Booking
+  def initialize(number, date, name, items = {}, elems = [], numbers = [])
+    @date = date
+    @number = number
+    @name = name
+    @items = items
+    @elems = elems
+    @numbers = numbers
+  end
+
+  attr_reader :date, :number, :name, :items, :elems, :numbers
+end
+
+class Container
+  def initialize(booking)
+    @booking = booking
+  end
+
+  attr_reader :booking
+end
+
 describe LightRulesEngine do
   it 'has a version number' do
     expect(LightRulesEngine::VERSION).not_to be nil
@@ -7,5 +28,155 @@ describe LightRulesEngine do
 
   it 'does something useful' do
     expect(false).to eq(true)
+  end
+
+  # $operator
+  # @value
+  # %const
+  #
+  let(:conditions_secound) do
+    {
+      kind: :operator,
+      type: :all,
+      values: [
+        {
+          kind: :operator,
+          type: :eq,
+          values: [
+            { kind: :data, type: 'booking.number' },
+            { kind: :value, type: :integer, value: 1 }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :gt,
+          values: [
+            { kind: :data, type: 'booking.number' },
+            { kind: :value, type: :integer, value: 0 }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :lt,
+          values: [
+            { kind: :data, type: 'booking.number' },
+            { kind: :value, type: :integer, value: 2 }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :range,
+          values: [
+            { kind: :value, type: :integer, value: 0 },
+            { kind: :value, type: :integer, value: 2 },
+            { kind: :data, type: 'booking.number' }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :eq,
+          values: [
+            { kind: :data, type: 'booking.items["a"].id' },
+            { kind: :value, type: :integer, value: 1 }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :eq,
+          values: [
+            { kind: :data, type: 'booking.elems[0].name' },
+            { kind: :value, type: :string, value: 'a' }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :eq,
+          values: [
+            { kind: :data, type: 'booking.elems.last.name' },
+            { kind: :value, type: :string, value: 'a' }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :eq,
+          values: [
+            { kind: :data, type: 'booking.numbers.max' },
+            { kind: :value, type: :integer, value: 300 }
+          ]
+        },
+        {
+          kind: :operator,
+          type: :any,
+          values: [
+            {
+              kind: :operator,
+              type: :eq,
+              values: [
+                { kind: :const, type: 'THE_NUMBER' },
+                { kind: :value, type: :integer, value: 3 }
+              ]
+            },
+            {
+              kind: :operator,
+              type: :eq,
+              values: [
+                { kind: :data, type: 'booking.name' },
+                { kind: :value, type: :string, value: 'test' }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  end
+
+  let(:conditions_simple) do
+    {
+      kind: :operator,
+      type: :eq,
+      values: [
+        { kind: :data, type: 'booking.number' },
+        { kind: :value, type: :integer, value: 1 }
+      ]
+    }
+  end
+
+  let(:conditions) do
+    {
+      '$_all' => [
+        { '$_eq' => ['@_booking.number', 1] },
+        { '$_gt' => ['@_booking.number', 0] },
+        { '$_lt' => ['@_booking.number', 2] },
+        {
+          '$any' => [
+            { '$_eq' => ['%_THE_NUMBER', 1] },
+            { '$_fluent_dates_eq' => ['$_booking.date', 'CHRISTMAS'] },
+            { '$_eq' => ['$_booking.name', 'test'] }
+          ]
+        }
+      ]
+    }
+  end
+
+  let(:shema) do
+    {
+      booking: {
+        kind: :object,
+        props: {
+          number: { kind: :integer },
+          date: { kind: :date_time },
+          name: { kind: :string }
+        }
+      }
+    }
+  end
+
+  let(:container) do
+    Container.new(Booking.new(1, Time.now, 'test', { 'a' => double('Test', id: 1) }, [double('ttt', name: 'a')], [1, 300, -1]))
+  end
+
+  it 'process' do
+    processor = LightRulesEngine::Processor.new(conditions: conditions_secound, data_container: container)
+    expect(processor.applable?).to eq(true)
   end
 end
